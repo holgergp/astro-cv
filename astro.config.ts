476329086a64126b4a943e309cf6597747c91ff6
Holger Grosse-Plankermann <holgergp@gmail.com>
2024-12-19 22:04:49 +0100

    chore(deps): upgrade all deps, while observing I may have a collection api to migrate

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,14 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
+import { SITE } from "@config";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  legacy: {
+    collections: true,
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
