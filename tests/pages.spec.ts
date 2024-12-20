bfb610ee73c3c21df9187ac6b424e8a031a31ed6
Holger Grosse-Plankermann <holgergp@gmail.com>
2024-12-20 21:05:39 +0100

    feat(playwright): some more structure

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -1,53 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, } from '@playwright/test';
 import percySnapshot from "@percy/playwright";
 
 test.describe('navigation', () => {
-  test('homepage', async ({ page, browserName }) => {
-    await page.goto('/');
+  ['homepage', 'career', 'buzzwords', 'publications', 'certifications', 'projects'].forEach((page) => {
+    test(`navigate to ${page}`, async ({ page, browserName }) => {
+      await page.goto(`/${page}`);
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Holger/);
-    await percySnapshot(page, `homepage ${browserName}`);
-  });
-
-  test('career', async ({ page, browserName }) => {
-    await page.goto('/career');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Holger/);
-    await percySnapshot(page, `career ${browserName}`);
-  });
-
-  test('buzzwords', async ({ page, browserName }) => {
-    await page.goto('/buzzwords');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Holger/);
-    await percySnapshot(page, `buzzwords ${browserName}`);
-  });
-
-  test('publications', async ({ page, browserName }) => {
-    await page.goto('/publications');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Holger/);
-    await percySnapshot(page, `publications ${browserName}`);
-  });
-
-  test('certifications', async ({ page, browserName }) => {
-    await page.goto('/certifications');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Holger/);
-    await percySnapshot(page, `certifications ${browserName}`);
-  });
-
-  test('projects', async ({ page, browserName }) => {
-    await page.goto('/projects');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Holger/);
-    await percySnapshot(page, `projects ${browserName}`);
+      await expect(page).toHaveTitle(/Holger/);
+      await percySnapshot(page, `navigate to ${page} ${browserName}`);
+    });
   });
 });
 
