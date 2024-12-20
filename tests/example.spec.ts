ce18417a863e2e81855644043274ec567444f0d2
Holger Grosse-Plankermann <holgergp@gmail.com>
2024-12-20 16:17:08 +0100

    feat(playwright): test percy

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
+import percySnapshot from "@percy/playwright";
 
 test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Holger/);
+  await percySnapshot(page, 'homepage');
 });
 
