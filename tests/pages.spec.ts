eeeef43cc9bb431231c478d5c709c03ad46cb7a7
Holger Grosse-Plankermann <holgergp@gmail.com>
2024-12-20 21:10:35 +0100

    feat(playwright): some special cases

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -2,13 +2,27 @@ import { test, expect, } from '@playwright/test';
 import percySnapshot from "@percy/playwright";
 
 test.describe('navigation', () => {
-  ['homepage', 'career', 'buzzwords', 'publications', 'certifications', 'projects'].forEach((page) => {
+  ['career', 'buzzwords', 'publications', 'certifications', 'projects'].forEach((page) => {
     test(`navigate to ${page}`, async ({ page, browserName }) => {
       await page.goto(`/${page}`);
 
       await expect(page).toHaveTitle(/Holger/);
-      await percySnapshot(page, `navigate to ${page} ${browserName}`);
+      await percySnapshot(page, `navigate to ${page} with ${browserName}`);
     });
+  });
+
+  test(`navigate to homepage`, async ({ page, browserName }) => {
+    await page.goto(`/`);
+
+    await expect(page).toHaveTitle(/Holger/);
+    await percySnapshot(page, `navigate to / with ${browserName}`);
+  });
+
+  test(`navigate to 404`, async ({ page, browserName }) => {
+    await page.goto(`/notthere`);
+
+    await expect(page).toHaveTitle(/Holger/);
+    await percySnapshot(page, `navigate to /404 with ${browserName}`);
   });
 });
 
